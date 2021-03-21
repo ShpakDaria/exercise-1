67adf1bcfa9aa78cf0b1147ed2af3ebb7a3b26a1
Шпак Дарья <78767999+ShpakDaria@users.noreply.github.com>
2021-03-21 10:00:39 +0900

    Update Main.java

--- exercise 1/src/com/company/Main.java
+++ exercise 1/src/com/company/Main.java
@@ -9,12 +9,12 @@ public class Main {
     public static void main(String[] args) {
 
         Random generator = new Random();
-        ArrayList<Cards> deck = new ArrayList<>();//ArrayList<Card> - масив карт3
+        ArrayList<Cards> deck = new ArrayList<>();//ArrayList<Card> 
         ArrayList<Chelovek> peoples = new ArrayList<>();
 
-        Scanner s = new Scanner(System.in);//создаём новый элемент класса который будет вводиться с консоли количество игроков
+        Scanner s = new Scanner(System.in);
         System.out.println("Введите число игроков: ");
-        int countPeoples = s.nextInt();// nextInt() это метод класса Scanner
+        int countPeoples = s.nextInt();
 
         if (countPeoples > 10) {
             System.out.println("Слишком много игроков, карточек не хватит");
@@ -22,25 +22,25 @@ public class Main {
         }
 
         for (int i = 0; i < countPeoples; i++) {
-            peoples.add(new Chelovek()); // загружаем количество людей определённых ранее в масив
+            peoples.add(new Chelovek()); 
         }
 
         for (int i = 0; i < Cards.SUITS_LIST.length; i++) {
             for (int j = 0; j < Cards.RANK_LIST.length; j++) {
                 deck.add(new Cards(Cards.SUITS_LIST[i], Cards.RANK_LIST[j]));
-            } // перебераем карты и добавляем их в колоду
+            } 
         }
 
-        for (Chelovek c : peoples) { // перебирая масив людей мы присваеваем значение "с" определённому человеку, так продвигаемся по всему масиву
+        for (Chelovek c : peoples) {
             for (int i = 0; i < 5; i++) {
-                Cards card = deck.get(generator.nextInt(deck.size()));//nextInt - случайное число от 0 до размера колоды текущего
-                c.getCards().add(card);//getCards()- текущий список кард, add(card)- добавляет новую случайную карту
+                Cards card = deck.get(generator.nextInt(deck.size()));
+                c.getCards().add(card);
                 deck.remove(card);
             }
         }
 
         for (Chelovek c : peoples) {
-            System.out.println(c.getCards().toString());// getCards().toString() список кард преобразуеться в формат строки
+            System.out.println(c.getCards().toString());
         }
         System.out.println("Осталось карт: " + deck.size());
     }
